2de4f09dfbff7802f26e8bcbc8323f0bd0916230
Lawrence Muema Kioko <lorenkioko@gmail.com>
2024-02-06 19:22:59 +0300

    Add navbar links

---
+++ src/components/navbar.jsx
@@ -0,0 +1,27 @@
+import MenuLink from "./menulink";
+
+const Navbar = () => (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+        <a className="navbar-brand" href="/">Logo</a>
+        <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+            <MenuLink url="/home" pageName="Home" />
+            </ul>
+        </div>
+        </div>
+    </nav>
+    );
+
+    export default Navbar;
