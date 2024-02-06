4dd2a39e269510643f16e25d67bdc8be2dfdbb33
Lawrence Muema Kioko <lorenkioko@gmail.com>
2024-02-06 19:21:54 +0300

    Add homepage section

---
+++ src/components/home/main.jsx
@@ -0,0 +1,16 @@
+const Main = () => {
+  return (
+    <div>
+      <h1>Digital solutions for your business</h1>
+      <p>
+        At our consultancy, we specialize in Digital Marketing, Web Development,
+        and Cybersecurity, providing tailored solutions to small businesses.
+        Partner with us to enhanve your oline presence and secure your digital
+        assetsy.
+      </p>
+      <button>More About Us</button>
+    </div>
+  );
+};
+
+export default Main;
