8bd84433be0bd9aadd6035f9dad7d5d0e2ddc0e3
Lawrence Muema Kioko <lorenkioko@gmail.com>
2024-02-06 22:21:52 +0300

    Add route links to the different pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './views/home';
+import AboutUs from './views/about_us';
+import Services from './views/services';
+import Contacts from './views/contacts';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="home" element={<Home />} />
+      <Route path="about_us" element={<AboutUs />} />
+      <Route path="services" element={<Services />} />
+      <Route path="contacts" element={<Contacts />} />
     </Routes>
   );
 }
