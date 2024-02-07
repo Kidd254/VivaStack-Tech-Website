8285698aa1bb58a89cdce8ce86f78905d5e55d84
Lawrence Muema Kioko <lorenkioko@gmail.com>
2024-02-07 16:20:41 +0300

    Configure Bootstrap for all components

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
