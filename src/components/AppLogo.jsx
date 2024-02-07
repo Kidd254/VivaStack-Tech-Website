1292c64434a7afaf1afd814965805d1c530781f3
Lawrence Muema Kioko <lorenkioko@gmail.com>
2024-02-08 00:30:17 +0300

    Create logo component to render the logo

---
+++ src/components/AppLogo.jsx
@@ -0,0 +1,10 @@
+import logo from '../assets/images/logos/logo.png';
+import '../assets/styles/AppLogo.css';
+
+const AppLogo = () => (
+  <div className="app-logo">
+    <img className="log" src={logo} alt="App Logo" />
+  </div>
+);
+
+export default AppLogo;
