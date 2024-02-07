7d9f603abffa73768352a643921aa190ff95e6c4
Lawrence Muema Kioko <lorenkioko@gmail.com>
2024-02-08 00:31:06 +0300

    Add img file and render it one the home page

--- src/components/home/main.jsx
+++ src/components/home/main.jsx
@@ -1,13 +1,23 @@
+/* eslint-disable react/button-has-type */
+import home from '../../assets/images/home/main-image.png';
+
 const Main = () => (
-  <div>
-    <h1>Digital solutions for your business</h1>
-    <p>
-      At our consultancy, we specialize in Digital Marketing, Web Development,
-      and Cybersecurity, providing tailored solutions to small businesses.
-      Partner with us to enhanve your oline presence and secure your digital
-      assetsy.
-    </p>
-    <button>More About Us</button>
+  <div className="container mt-4">
+    <div className="row">
+      <div className="col-md-6">
+        <h1>Digital solutions for your business</h1>
+        <p>
+          At our consultancy, we specialize in Digital Marketing, Web
+          Development, and Cybersecurity, providing tailored solutions to small
+          businesses. Partner with us to enhanve your oline presence and secure
+          your digital assets.
+        </p>
+        <button>More About Us</button>
+      </div>
+      <div className="col-md-6">
+        <img src={home} alt="placeholder" />
+      </div>
+    </div>
   </div>
 );
 
